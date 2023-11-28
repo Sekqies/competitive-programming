01afd1c0b22b56a55cfaa4f2e55da9a55da779c5
Sekqies <fernando.ellerbrock@gmail.com>
2023-11-27 22:19:05 -0300

    Aplicado o algoritmo de Kruskal na OBI 2014

--- OBI/2014/Fase 1/2014_Fase1_copa.cpp
+++ OBI/2014/Fase 1/2014_Fase1_copa.cpp
@@ -1,32 +1,75 @@
-#include <iostream>
-#include <vector>
-#include <utility>
-#define mp make_pair
+#include <bits/stdc++.h>
 using namespace std;
-typedef pair<int,int> pii;
-vector<pii> rodo[1010];
-vector<pii> ferro[1010];
 
-void dfs (int atual)
+const int MAXN = 10e5;
+
+int pai[MAXN];
+int peso[MAXN] = {0};
+vector<array<int,4>> caminhos;
+
+
+int find(int x)
 {
-	
+	if (pai[x] == x)
+		return x; 
+	else
+		return pai[x] = find(pai[x]);
 }
+bool join(int x, int y)           
+{
+	x = find(x);
+	y = find(y);
+
+	if (x==y)
+		return false;
+	if(peso[x] > peso[y])
+		pai[y] = x;
+	else if (peso[x] < peso[y])
+		pai[x] = y;
+	
+	else if (peso[x] == peso[y])
+	{
+		pai[x] = y;
+		peso[y]++;
+	}
+	return true;
+}
+
+bool sortPerso(array<int,4> x, array<int,4> y)
+{
+	if (x[0] != y[0])
+		return x[0] < y[0];
+	else
+		return x[3] < y[3];
+}
+
 int main()
 {
-	int N,R,F;
-	cin >> N >> F >> R;
-	for (int i=0; i<F; i++)
+	int n,r,f;
+	cin >> n >> f >> r;
+	for (int i=1; i<=n; i++)
 	{
-		int A,B,C;
-		cin >> A >> B >> C;
-		rodo[A].push_back(mp(B,C));
-		rodo[B].push_back(mp(A,C));
+		pai[i]=i;
 	}
-	for (int i=0; i<F; i++)
+	for (int i=0; i<f; i++)
 	{
-		int A,B,C;
-		cin >> A >> B >> C;
-		ferro[A].push_back(mp(B,C));
-		ferro[B].push_back(mp(A,C));
+		int a,b,c;
+		cin >> a >> b >> c;
+		caminhos.push_back({1,a,b,c} );
 	}
+	for (int i=0; i<r;i++)
+	{
+		int a,b,c;
+		cin >> a >> b >> c;
+		caminhos.push_back({2,a,b,c} );
+	}
+	sort(caminhos.begin(),caminhos.end(),sortPerso);
+	 int res = 0;
+	for (const auto& cam : caminhos)
+	{
+		if (join(cam[1],cam[2]))
+			res+= cam[3];
+	}
+	
+	cout << res;
 }
